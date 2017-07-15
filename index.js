70110d09b0690b305f2b9d43f11b03563712debb
Mike Zou <zoutianh@grinnell.edu>
2017-07-14 21:21:35 -0500

    logging mongodb file

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const mongo = require('mongodb').MongoClient;
 const app = express();
 
-app.use(express.static('public'));
+var remoteurl = "ec2-13-58-191-238.us-east-2.compute.amazonaws.com:27017"
+
+app.get('/', function(err, req, res) {
+  console.log("haha");
+  mongo.connect(remoteurl, function(err, db) {
+    if (!err) {
+      console.log("We are connected!");
+    }
+  });
+});
+
+//app.use(express.static('public'));
+
 app.listen(3000, () => console.log('Server running on port 3000'));
