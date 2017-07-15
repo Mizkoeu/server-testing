31d3f3a7cf7c24f6fd862f9c7f0f3041f1c0833d
Mike Zou <zoutianh@grinnell.edu>
2017-07-14 21:42:29 -0500

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 var remoteurl = "ec2-13-58-191-238.us-east-2.compute.amazonaws.com:27017"
 
 app.get('/', function(err, req, res) {
+  res.render('public/index.html', {});
   console.log("haha");
   mongo.connect(remoteurl, function(err, db) {
     if (!err) {
